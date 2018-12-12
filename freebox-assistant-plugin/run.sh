23334c0488aa80877c43668ad9bf57671edf239e
frazrepo <frazvisuals@gmail.com>
2018-12-12 11:17:48 +0100

    Update run.sh

--- freebox-assistant-plugin/run.sh
+++ freebox-assistant-plugin/run.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
+CONFIG_PATH=/data/options.json
+
 version=$(node -v)
 echo "[fbx-addon] Checking node version : $version"
+
+echo "[fbx-addon] Generating freebox plugin configuration file..."
+python3 config_builder.py "$CONFIG_PATH"
 
 echo "[fbx-addon] Starting Freebox Assistant Config Add-on..."
 node index.js
